a61d5f115426a49ae6952e5f7dae58ac8886d274
Bruno Lollato <bruno.andrade.ext@infracommerce.com.br>
2022-08-10 10:41:20 -0300

    itens per page 25

--- src/utils/getServerSideProps.ts
+++ src/utils/getServerSideProps.ts
@@ -2,7 +2,7 @@ import { getPokemons, getPokemonData } from '../api/api';
 import { Pokemon } from '../types/global';
 
 export default async function getServerSideProps(ctx: any) {
-  const itensPerPage = 35;
+  const itensPerPage = 25;
   const page = 0;
   const res = await getPokemons(itensPerPage, itensPerPage * page);
   const promises = res?.results?.map(async (pokemon: {url: string}) => {
